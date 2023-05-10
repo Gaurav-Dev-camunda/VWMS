121d33871f37c11d140c1a9793d0b7ef692660af
TN176NT <TN176NT@IN3260690W1.ey.net>
2023-05-10 13:55:25 +0530

    Initial commit

---
+++ vehicle-wash-management-system/src/main/java/com/cloudverse/vehiclewashmanagementsystem/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.cloudverse.vehiclewashmanagementsystem;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "vehicle-wash-management-system"; // BPMN Process ID
+
+}
